bcd5078117ee39382862cb20b754dddcce9c5f90
andiespo <andiespo@gmail.com>
2021-09-13 22:32:58 -0300

    update carpetas

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import NavBar from "./Components/NavBar"
+
+const App = () => 
+        <header className="container-fluid">
+                <h1>Bienvenidos a The Healthy APP</h1>
+                <NavBar/>
+        </header>
+
+export default App
