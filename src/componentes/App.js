cb254bbdcfaaa7db67e9a078b4eb2a65e81ad850
andiespo <andiespo@gmail.com>
2021-09-13 22:38:21 -0300

    updates

--- src/componentes/App.js
+++
@@ -1,9 +0,0 @@
-import NavBar from "./NavBar"
-
-const App = () => 
-        <header className="container-fluid">
-                <h1>Bienvenidos a The Healthy APP</h1>
-                <NavBar/>
-        </header>
-
-export default App
