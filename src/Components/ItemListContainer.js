c4335663b70526c44842ea7ffef7078a17e202f4
andiespo <andiespo@gmail.com>
2021-09-15 21:15:10 -0300

    agregando shopping cart e itemlistcontainer

---
+++ src/Components/ItemListContainer.js
@@ -0,0 +1,13 @@
+const ItemListContainer = ({titulo, subtitulo }) => {
+
+    return (
+        <div className="Item" id={"id"} >
+            <h1>{titulo}</h1>
+            <p>{subtitulo}</p>
+        </div>
+    )
+}
+
+
+
+export default  ItemListContainer
