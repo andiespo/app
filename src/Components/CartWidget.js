9b7f847dcbb3d48828d98969e9fbbbe270a70096
andiespo <andiespo@gmail.com>
2021-09-20 22:17:52 -0300

    update

---
+++ src/Components/CartWidget.js
@@ -0,0 +1,12 @@
+const CartWidget= ({titulo, subtitulo }) => {
+
+    return (
+        <div className="CartWidget" id={"id"} >
+            <h1>{titulo}</h1>
+            <p>{subtitulo}</p>
+        </div>
+    )
+}
+
+
+export default CartWidget
