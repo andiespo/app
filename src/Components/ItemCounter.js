b3db05b2e3e998c027a6d11c251b0885c1e6b438
andiespo <andiespo@gmail.com>
2021-09-20 21:13:07 -0300

    update

--- src/Components/ItemCounter.js
+++ src/Components/ItemCounter.js
@@ -1,13 +1,13 @@
 const ItemCounter = ({titulo}) => {
 
-    const [contador,setContador] = useState()
+    const [contador,setContador] = useState(0)
     let titulo = "Mi E-commerce"
     
     return (
         <div className="ItemCounter" id={"id"} >
             <h1>{titulo}</h1>
             <p>Contador:{contador}</p>
-            <button onClick= {()=> {onAdd ()}}> aumentar contador  </button>
+            <button onClick= {()=> {onAdd(setContador(contador+1))}}> aumentar contador  </button>
             <contador onAdd ={}/>
         </div>
     )
