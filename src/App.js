7fc0104891f37c0190244cd1475b19a7aa63617d
andiespo <andiespo@gmail.com>
2021-09-28 21:28:07 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Header from "./Components/Header"
-import ItemListContainer from "./Components/ItemListContainer"
-import ItemDetailContainer from "./Components/ItemDetailContainer"
 import Footer from "./Components/Footer"
 const App = () => {
 
@@ -9,8 +7,6 @@ const App = () => {
         return (
         <>
         <Header id ="main-header" titulo= "The Healthy APP" Subtitulo= "Mi E-commerce"/>
-        <ItemListContainer/>
-        <ItemDetailContainer/>
         <Footer/>
         </>
         )
